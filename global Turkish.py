ff7d110a91e2efd244ddab7f0d16819e6434ffc7
Doğa Berk Kömür <dogakomur991@gmail.com>
2024-09-23 19:30:16 +0300

    Discord bot

---
+++ global Turkish.py
@@ -0,0 +1,44 @@
+global Turkish
+By the way script is in down
+hasattr = set lambda
+breakpoint case try type
+TabError
+break
+StopAsyncIteration = TabError 
+function property open
+Immotinal = property
+hasattr ("red")
+finally = terminal
+NotADirectoryError
+set and ConnectionAbortedError
+KeyboardInterrupt nonlocal None
+the biggest command == while True
+
+
+
+
+
+while True:
+    print(terminal)
+tuple ImportError jjsploit
+license
+==
+True break vars set hasattr
+hasattr global hasattr
+set match
+or nonlocal
+finally
+nonlocal ç,match
+vars yield nonlocal
+hasattr hasattr
+lambda jjsploit = taş madendir for luck bruhhh
+def hashtag
+KeyboardInterrupt = True
+match and hasattr try open TabError assert import set vars vars
+match KeyboardInterrupt KeyboardInterrupt in import filter or FileNotFoundError keyerror:
+    sorted elif def
+    hasattr hasattr
+    do just some function
+    function == True
+    hasattr
+    try
